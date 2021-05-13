449a53dd36cfabffb7ed29604584192765e4de94
ChiShen-Chao <jasonjohnny258@gmail.com>
2021-05-14 01:24:37 +0900

    reload

--- Opencv_Chapter1.py
+++ Opencv_Chapter1.py
@@ -1,19 +1,20 @@
+# load images, videos or webcam
 import cv2
-'''
-#test opencv install success
-print("Package Imported")
-'''
 
+#print("Package Imported")   #test opencv install success
+
+#=====load image==========
 '''
-# picture load and print test
 img = cv2.imread(".jpg")
 
 cv2.imshow("Output", img)
 cv2.waitKey(0)
 '''
+
+#=====load video==========
 '''
-# load video and webcam is the same process the only different is the "path" "video.mp4" or 0
 cap = cv2.VideoCapture(".mp4")
+
 
 while True:
     success, img = cap.read()
@@ -23,6 +24,7 @@ while True:
         break
 '''
 
+#=====webcam==========
 # 0 means the default webcam;
 # 3 = width, 4 = height
 cap = cv2.VideoCapture(0)
